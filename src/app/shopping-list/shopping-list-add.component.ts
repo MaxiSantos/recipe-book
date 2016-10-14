fc7e555553a7573c50517d916ed52515403a128b
maxi <rpenaranda@belatrixsf.com>
2016-10-14 09:59:37 -0300

    Adding shopping-list-add

--- src/app/shopping-list/shopping-list-add.component.ts
+++ src/app/shopping-list/shopping-list-add.component.ts
@@ -1,14 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, OnInit, Input } from '@angular/core';
+import { Ingredient } from '../shared/ingredient';
+import { ShoppingListService } from './shopping-list.service';
 
 @Component({
   selector: 'rb-shopping-list-add',
   templateUrl: './shopping-list-add.component.html'
 })
-export class ShoppingListAddComponent implements OnInit {
+export class ShoppingListAddComponent implements OnChanges, OnInit {
+  isAdding = true;
+  @Input() item: Ingredient;
 
-  constructor() { }
+  constructor(private sls: ShoppingListService) { }
 
-  ngOnInit() {
+  ngOnChanges(changes) {
+    this.isAdding = changes.item.currentValue === null;
+    this.isAdding ? this.item = {name: null, amount: null} : null;
   }
 
+  ngOnInit(){
+    console.log(this.item);
+  }
+
+  onSubmit(ingredient: Ingredient){
+    const newIngredient = new Ingredient(ingredient.name, ingredient.amount);
+    if(!this.isAdding) {
+      this.sls.editItem(this.item, newIngredient)
+    } else {
+      this.item = newIngredient;
+      this.sls.addItem(this.item);
+    }
+  }
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -7,11 +7,18 @@ import { ShoppingListService } from './shopping-list.service';
   templateUrl: './shopping-list.component.html'
 })
 export class ShoppingListComponent implements OnInit {
-  public items: Ingredient[]
+  items: Ingredient[];
+  selectedIngredient: Ingredient = null; // not undefined, null to specify not selected item
+  //selectedItem: Ingredient = null; // not undefined, null to specify not selected item
+
   constructor(private sls: ShoppingListService) {}
 
   ngOnInit() {
     this.items = this.sls.getItems();
+  }
+
+  onSelectItem(item: Ingredient){
+    this.selectedIngredient = item;
   }
 
 }
